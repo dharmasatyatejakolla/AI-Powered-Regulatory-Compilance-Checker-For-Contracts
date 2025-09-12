30d09bebc233ed6380f346ab301c3c1bab581aab
Dharma Satya Teja Kolla <dharmasatyatejakolla@gmail.com>
2025-09-12 21:22:51 +0530

    updated risk_assessment module

--- risk_assessment/__init__.py
+++ risk_assessment/__init__.py
@@ -1,9 +1,10 @@
-from .extract_pdf import extract_clauses
-from .analyze_clauses import analyze_batch
+from .analyze_clauses import analyze_batch, analyze_all_batches
 from .ingestion_processing import ingest_to_sheet
+from .extract_pdf import extract_clauses
 
 __all__ = [
     "extract_clauses",
     "analyze_batch",
+    "analyze_all_batches",
     "ingest_to_sheet"
 ]
