9140b3c8b4019c42068a5fd57ef47e949d155598
Dharma Satya Teja Kolla <dharmasatyatejakolla@gmail.com>
2025-10-04 18:20:13 +0530

    Update __init__.py

--- risk_assessment/__init__.py
+++ risk_assessment/__init__.py
@@ -1,10 +1,16 @@
-from .analyze_clauses import analyze_batch, analyze_all_batches
-from .ingestion_processing import ingest_to_sheet
+"""
+risk_assessment package
+Provides utilities for extracting, analyzing, ingesting, and notifying about contract compliance risks.
+"""
+
 from .extract_pdf import extract_clauses
+from .analyze_clauses import analyze_all_batches
+from .ingestion_processing import ingest_to_sheet
+from .notification_alert import send_compliance_alert
 
 __all__ = [
     "extract_clauses",
-    "analyze_batch",
     "analyze_all_batches",
-    "ingest_to_sheet"
+    "ingest_to_sheet",
+    "send_compliance_alert",
 ]
